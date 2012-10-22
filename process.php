d7ae2f5f1395257a3d6d0c868658f6cba1b1c956
James Dressel <ubergeek@jamesdressel.com>
2012-10-21 21:08:22 -0400

    Add code for process.php to return results from the server

---
+++ process.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="derporiaStyle.css">
+<title>Your Assertion</title>
+</head>
+<body>
+<p>You claim <?php echo $_POST["claim"]; ?>, and wish to prove it with the following assertions: <?php echo $_POST["assertions"]; ?></p>
+</body>
+</html> 
