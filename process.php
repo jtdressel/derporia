764714d0531482a7c18499d6092cf290af67fe98
James Robertson <ubergeek@jamesrobertson.org>
2012-10-22 23:29:50 -0400

    change php formatting, upload to server
    because pretty

--- process.php
+++ process.php
@@ -4,6 +4,16 @@
 <title>Your Assertion</title>
 </head>
 <body>
-<p>You claim <?php echo $_POST["claim"]; ?>, and wish to prove it with the following assertions: <?php echo $_POST["assertions"]; ?></p>
+<p>
+	You claim:
+	<br>
+	<b><font color="#5F04B4"><?php echo $_POST["claim"]; ?></font></b>
+	<br><br>
+	And wish to prove it with the following assertions: 
+	<br>
+	<b><font color="#5F04B4"><?php echo $_POST["assertions"]; ?></font></b>
+</p>
+
+
 </body>
 </html> 
